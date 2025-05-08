d64b553d98ecd0f0dbc070455628f6672f8f6c5c
parsa <parsa.miraghaie@gmail.com>
2025-05-08 12:00:41 +0300

    url link with decorator works now but only for sync

--- pyapp/src/pyapp/observation/instance.py
+++ pyapp/src/pyapp/observation/instance.py
@@ -1,16 +1,19 @@
 from loguru import logger
+import requests
 from taskpy import TaskCLI
 from pathlib import Path
 tracer = None
 
 class PhoenixObservation:
+  _port = 6006
+  _grpc_port = 4317
   @property
   def port(self):
-    return 6006
+    return self._port
 
   @property
   def grpc_port(self):
-    return 4317
+    return self._grpc_port
 
   def __init__(self):
     self.task = TaskCLI(Path(__file__).parent )
@@ -21,7 +24,7 @@ class PhoenixObservation:
         answer = self.task.run("status")
         return int(answer.stdout.strip())!= 0
       except Exception as e:
-        logger.error(f"Error in phoenix observation: {e}")
+        # logger.error(f"Error in phoenix observation: {e}")
         return False 
 
   def start(self):
@@ -50,16 +53,27 @@ class PhoenixObservation:
     except Exception as e:
       logger.error(f"Error in phoenix observation: {e}")
 
-
-class PhonexLangChainInstrumentor:
+class PhoenixLangChainInstrumentor:
   _instance = None
   tracer_provider = None
+  project_url = None
   
   def __new__(cls, project_name: str = "lmorbits-phoenix", app_name: str = "app1"):
     if cls._instance is None:
       cls._instance = super().__new__(cls)
       cls._instance._initialized = False
     return cls._instance
+
+  @staticmethod
+  def get_project_url(project_name: str = "lmorbits-phoenix"):
+    response = requests.get(
+        f"http://localhost:6006/v1/projects/{project_name}",
+        headers={"Accept":"*/*"},
+    )
+
+    data = response.json()
+    project_name= data["data"]["id"]
+    return f"http://localhost:{PhoenixObservation._port}/projects/{project_name}"
 
   def __init__(self, project_name: str = "lmorbits-phoenix", app_name: str = "app1"):
     if not self._initialized:
@@ -78,6 +92,6 @@ class PhonexLangChainInstrumentor:
         LangChainInstrumentor().instrument(tracer_provider=self.tracer_provider)
         trace.set_tracer_provider(self.tracer_provider)
         self._initialized = True
-
+        self.project_url = self.get_project_url(self.project_name)
       except Exception as e:
         logger.error(f"Error in phoenix observation: {e}")
--- pyapp/src/pyapp/observation/phoneix.py
+++ pyapp/src/pyapp/observation/phoneix.py
@@ -1,11 +1,15 @@
-from .instance import PhonexLangChainInstrumentor, PhonexObservation
+from .instance import PhoenixLangChainInstrumentor, PhoenixObservation
 from openinference.instrumentation import using_session
 from openinference.semconv.trace import SpanAttributes
 from opentelemetry import trace # wherever your context manager lives
 from functools import wraps
+from pyapp.log.log_config import get_logger
+import base64
+logger = get_logger()
 
-PhonexLangChainInstrumentor()
-observation = PhonexObservation()
+ph_instrumentor = PhoenixLangChainInstrumentor()
+observation = PhoenixObservation()
+logger.info("Starting Phoenix observation")
 observation.start()
 
 def traced_agent(name: str, propagate_session: bool = True, tracer_name:str="lmorbits-trace"):
@@ -24,6 +28,14 @@ def traced_agent(name: str, propagate_session: bool = True, tracer_name:str="lmo
                 attributes={SpanAttributes.OPENINFERENCE_SPAN_KIND: "agent"}
             ) as span:
                 # record session and input
+                trace_id = span.get_span_context().trace_id
+                span_id = span.get_span_context().index(0)
+                text = f"Span:{span_id}"
+                st_big = base64.b64encode(text.encode("utf-8")).decode("utf-8")
+                trace_id_hex = format(trace_id, "032x")  # Converts to 32-character hex
+                
+                trace_url = f"{ph_instrumentor.project_url}/traces/{trace_id_hex}?selectedSpanNodeId={st_big}"
+                
                 span.set_attribute(SpanAttributes.SESSION_ID, session_id)
                 last_msg = messages[-1].get("content")
                 span.set_attribute(SpanAttributes.INPUT_VALUE, last_msg)
@@ -40,7 +52,7 @@ def traced_agent(name: str, propagate_session: bool = True, tracer_name:str="lmo
                 output = getattr(result, "content", result)
                 span.set_attribute(SpanAttributes.OUTPUT_VALUE, output)
 
-                return result
+                return result,trace_url
         return wrapper
     return decorator
 
