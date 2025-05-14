3e508aca4992edbe668a042b19dd5ae046fbaef3
parsa <parsa.miraghaie@gmail.com>
2025-05-14 12:19:23 +0300

    get_data_dir added

--- pyapp/src/pyapp/config.py
+++ pyapp/src/pyapp/config.py
@@ -10,4 +10,24 @@ def get_config(config_path:str):
         config = toml.load(f)
     return Config(**config)
 
+def find_config(current_file: str, config_name: str = "appdeps.toml"):
+    config_path = Path(current_file).resolve()
+    root = Path(config_path.root)
+    for parent in [config_path] + list(config_path.parents):
+        config_file = parent / config_name
+        if config_file.exists():
+            return config_file
+        if parent == root:
+            break
+    raise FileNotFoundError(f"Config file not found: {config_name}")
+
+def get_data_dir(current_file: str):
+    config_file = find_config(current_file)
+    config = get_config(config_file.parent)
+    if config.vectordb:
+        return config_file.parent / config.vectordb.inRepoPath
+    else:
+        raise ValueError("No vector store found")
+
+
 
