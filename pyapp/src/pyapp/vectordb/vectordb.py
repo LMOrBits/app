83fa2bab74c12fdfb1068896240df85ebdc3045f
parsa <parsa.miraghaie@gmail.com>
2025-05-14 11:39:57 +0300

    fixed the bug

--- pyapp/src/pyapp/vectordb/vectordb.py
+++ pyapp/src/pyapp/vectordb/vectordb.py
@@ -2,10 +2,11 @@ import duckdb
 from langchain_community.vectorstores import DuckDB
 from pathlib import Path
 
-def get_vector_store(database_path: Path , table_name: str , embedding_function: str):
+def get_vector_store(database_path: Path|str , table_name: str , embedding_model: str):
     """
     Get the vector store from the database path.
     """
+    database_path = str(database_path)
     if not database_path.endswith(".db"):
         raise ValueError("database_path must end with .db")
     conn = duckdb.connect(database=database_path,
@@ -15,4 +16,4 @@ def get_vector_store(database_path: Path , table_name: str , embedding_function:
                                   "autoload_known_extensions": "false"
                               }
 )
-    return DuckDB(connection=conn ,table_name=table_name, embedding_function=embedding_function)
+    return DuckDB(connection=conn ,table_name=table_name, embedding=embedding_model)
